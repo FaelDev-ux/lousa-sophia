c213d83a56d66056d0457afae5d35a0e741b2178
Samuel Medeiros <84237653+samuelmedeirosjs@users.noreply.github.com>
2026-03-14 17:59:37 -0300

    Add onClick handler to Limpar button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ onClear }) {
     <header className="w-full bg-gray-50 gap-4 px-4 flex items-center justify-between self-start">
       <img className="max-w-30" src={logo} alt="logo Sophia" />
       <SophiaBar />
-      <Button type="delete" text="Limpar" />
+      <Button type="delete" text="Limpar" onClick={onClear}/>
     </header>
   );
 }
